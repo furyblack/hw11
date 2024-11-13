43260953af808b62fab07428af98029e0d3f6d60
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-11-13 16:01:26 +0500

    refactor comment router to classes

--- src/routes/comment-router.ts
+++ src/routes/comment-router.ts
@@ -13,97 +13,110 @@ export const commentRouter= Router({})
 //миддл вар для получения комента всем пользователям( и даже не авторизованным)
 
 
-commentRouter.get('/:id', extractUserIdFromToken, async (req: Request, res: Response) => {
-    const commentId = req.params.id;
-    const userId = req.userDto ? req.userDto._id.toString() : null;
+class CommentController{
+    async getComment(req: Request, res: Response){
+        {
+            const commentId = req.params.id;
+            const userId = req.userDto ? req.userDto._id.toString() : null;
 
-    const comment = await QueryCommentRepository.getById(commentId);
+            const comment = await QueryCommentRepository.getById(commentId);
 
-    if (!comment) {
-        return res.sendStatus(404);
-    }
+            if (!comment) {
+                return res.sendStatus(404);
+            }
 
-    let myStatus = 'None';
+            let myStatus = 'None';
 
-    if (userId) {
-        const userLike = await LikeModel.findOne({ commentId, userId });
-        if (userLike) {
-            myStatus = userLike.status;
+            if (userId) {
+                const userLike = await LikeModel.findOne({ commentId, userId });
+                if (userLike) {
+                    myStatus = userLike.status;
+                }
+            }
+
+            const responseComment = {
+                id: comment.id,
+                content: comment.content,
+                commentatorInfo: comment.commentatorInfo,
+                createdAt: comment.createdAt,
+                likesInfo: {
+                    likesCount: comment.likesInfo.likesCount,
+                    dislikesCount: comment.likesInfo.dislikesCount,
+                    myStatus: myStatus,
+                }
+            };
+
+            res.status(200).send(responseComment);
+            return
         }
     }
+    async  updateComment(req: Request, res: Response){
+        {
+            const commentUpdateParams: UpdateCommentType={
+                content: req.body.content
+            }
+            const commentId = req.params.id
 
-    const responseComment = {
-        id: comment.id,
-        content: comment.content,
-        commentatorInfo: comment.commentatorInfo,
-        createdAt: comment.createdAt,
-        likesInfo: {
-            likesCount: comment.likesInfo.likesCount,
-            dislikesCount: comment.likesInfo.dislikesCount,
-            myStatus: myStatus,
+            const userId = req.userDto._id
+
+            const foundComment = await CommentRepository.findById(commentId)
+            if( foundComment && foundComment?.commentatorInfo.userId.toString() !== userId.toString()){
+                return res.sendStatus(403)
+            }
+            await CommentRepository.updateComment(commentId, commentUpdateParams)
+            if(!foundComment) return res.sendStatus(404)
+            return res.sendStatus(204)
         }
-    };
-
-    res.status(200).send(responseComment);
-    return
-});
-
-
-
-commentRouter.put('/:id', authMiddlewareBearer, commentForPostValidation(), async (req: Request, res: Response) =>{
-    const commentUpdateParams: UpdateCommentType={
-        content: req.body.content
     }
-    const commentId = req.params.id
+    async  deleteComment(req:Request,res:Response){
+        {
+            const commentId = req.params.id
 
-    const userId = req.userDto._id
+            const userId = req.userDto._id
 
-    const foundComment = await CommentRepository.findById(commentId)
-    if( foundComment && foundComment?.commentatorInfo.userId.toString() !== userId.toString()){
-        return res.sendStatus(403)
-    }
-    await CommentRepository.updateComment(commentId, commentUpdateParams)
-    if(!foundComment) return res.sendStatus(404)
-        return res.sendStatus(204)
-})
+            const foundComment = await CommentRepository.findById(commentId)
+            if (foundComment && foundComment?.commentatorInfo.userId.toString() !== userId.toString()) {
+                return res.sendStatus(403)
+            }
 
-commentRouter.delete('/:id',authMiddlewareBearer, async (req:Request,res:Response) =>{
-
-    const commentId = req.params.id
-
-    const userId = req.userDto._id
-
-    const foundComment = await CommentRepository.findById(commentId)
-    if(foundComment && foundComment?.commentatorInfo.userId.toString() !== userId.toString()){
-        return res.sendStatus(403)
-    }
-
-    await CommentRepository.deleteComment(commentId)
-    if(!foundComment) return res.sendStatus(404)
-     return res.sendStatus(204)
-})
-
-commentRouter.put('/:id/like-status', authMiddlewareBearer, async (req:Request,res:Response)=>{
-    const id = req.params.id
-    const  {likeStatus} = req.body
-    const userId = req.userDto._id.toString()
-
-    if(!['None', 'Like','Dislike'].includes(likeStatus)){
-        return res.status(400).send({errorsMessages:[{message:'Invalid like status', field:'likeStatus'}]})
-    }
-
-    try {
-        const commentsExists = await CommentModel.findById(id)
-        if(!commentsExists){
-            return res.status(404).send({errorMessages:[{message:'Comment not found', field:'commentId'}]})
+            await CommentRepository.deleteComment(commentId)
+            if (!foundComment) return res.sendStatus(404)
+            return res.sendStatus(204)
         }
-
-        await CommentService.updateLikeStatus(id, userId, likeStatus)
-        return res.sendStatus(204)
-    }catch (error){
-        console.error('Error updating like status:', error);
-        return res.status(500).send({error:'Something went wrong'});
     }
-})
+    async updateLikeStatus(req:Request,res:Response){
+        {
+            const id = req.params.id
+            const  {likeStatus} = req.body
+            const userId = req.userDto._id.toString()
+
+            if(!['None', 'Like','Dislike'].includes(likeStatus)){
+                return res.status(400).send({errorsMessages:[{message:'Invalid like status', field:'likeStatus'}]})
+            }
+
+            try {
+                const commentsExists = await CommentModel.findById(id)
+                if(!commentsExists){
+                    return res.status(404).send({errorMessages:[{message:'Comment not found', field:'commentId'}]})
+                }
+
+                await CommentService.updateLikeStatus(id, userId, likeStatus)
+                return res.sendStatus(204)
+            }catch (error){
+                console.error('Error updating like status:', error);
+                return res.status(500).send({error:'Something went wrong'});
+            }
+        }
+    }
+}
+const commentController = new CommentController()
+
+commentRouter.get('/:id', extractUserIdFromToken, commentController.getComment);
+
+commentRouter.put('/:id', authMiddlewareBearer, commentForPostValidation(), commentController.updateComment)
+
+commentRouter.delete('/:id',authMiddlewareBearer, commentController.deleteComment)
+
+commentRouter.put('/:id/like-status', authMiddlewareBearer, commentController.updateLikeStatus)
 
 
